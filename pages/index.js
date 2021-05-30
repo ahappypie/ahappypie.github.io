4c8f1b37bc036ac9ba3dc3b966afa09605453bd4
Brian Bagdasarian <bbagdasarian.scs@gmail.com>
2021-05-29 21:13:21 -0700

    Migrate to next.js/tailwindcss

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import Container from '@/components/container';
+
+export default function Home() {
+    return (
+        <Container>
+            <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
+                <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
+                    Hi, I’m Brian Bagdasarian
+                </h1>
+                <h2 className="prose text-gray-600 dark:text-gray-400 mb-16">
+                    I help solve fundamental problems. Right now, I work as a Big Data Engineer
+                    at Nativo on next generation data infrastructure. This site is a little look at the work I do
+                    –&nbsp;
+                    <Link href="/projects">
+                        <a>take a look at some of my projects</a>
+                    </Link>
+                    {" or "}
+                    <Link href="/about">
+                        <a>or learn more about me.</a>
+                    </Link>
+                </h2>
+            </div>
+        </Container>
+    )
+}
